f56b8a4d2c8a41b3bfce5133d178776dc95bde89
Rashminda <rashminda121@gmail.com>
2024-02-09 21:02:13 +0530

    Update AdminLogin.java

--- src/main/java/com/mycompany/petmanagment/AdminLogin.java
+++ src/main/java/com/mycompany/petmanagment/AdminLogin.java
@@ -21,7 +21,6 @@ public class AdminLogin extends javax.swing.JFrame {
     ResultSet rs=null;
     Statement st=null;
     
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
