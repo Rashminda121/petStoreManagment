cb5afde6c705e5c69cbdcc1091ffe0c9908ec856
Rashminda <rashminda121@gmail.com>
2024-02-09 21:04:20 +0530

    Update AdminLogin.java

--- src/main/java/com/mycompany/petmanagment/AdminLogin.java
+++ src/main/java/com/mycompany/petmanagment/AdminLogin.java
@@ -228,9 +228,7 @@ public class AdminLogin extends javax.swing.JFrame {
            this.dispose();
     }//GEN-LAST:event_jLabel10MouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
